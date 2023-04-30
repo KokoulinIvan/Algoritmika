b8e9a207d812ba4ab31ee4a0fb0f1efb25730b30
KokoulinIvan <131598455+KokoulinIvan@users.noreply.github.com>
2023-04-30 19:01:16 +0500

    Update my_app.py

--- my_app.py
+++ my_app.py
@@ -13,26 +13,3 @@ main_win.resize(400, 200)
 but = QPushButton('1')
 but1 = QPushButton('2')
 text1 = QLabel('')
-
-h_line = QHBoxLayout()
-h_line.addWidget(but, alignment = Qt.AlignCenter)
-h_line.addWidget(but1, alignment = Qt.AlignCenter)
-main_win.setLayout(h_line)
-
-
-
-def win():
-    win1 = QMessageBox()
-    win1.setText('Победа!')
-    win1.exec_()
-
-def lose():
-    lose1 = QMessageBox()
-    lose1.setText('Проигрыш :(')
-    lose1.exec_()
-
-but.clicked.connect(win)
-but1.clicked.connect(lose)
-
-main_win.show()
-app.exec_()
