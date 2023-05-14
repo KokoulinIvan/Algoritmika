36e2e345155df6133207e9ccef1195496dfa40cd
KKkkksen <133582148+KKkkksen@users.noreply.github.com>
2023-05-14 18:53:20 +0500

    Update my_app.py
    ver 3(Ксюша)

--- my_app.py
+++ my_app.py
@@ -1,58 +1,48 @@
-from PyQt5.QtCore import Qt
-from PyQt5.QtWidgets import QApplication, QWidget, QPushButton,QLabel, QVBoxLayout, QHBoxLayout
+from PyQt5.QtCore import Qt, QTimer, QTime, QLocale
+from PyQt5.QtGui import QDoubleValidator, QIntValidator, QFont 
+from PyQt5.QtWidgets import (
+        QApplication, QWidget,
+        QHBoxLayout, QVBoxLayout, QGridLayout,
+        QGroupBox, QRadioButton,
+        QPushButton, QLabel, QListWidget, QLineEdit)
 from instr import *
+from final_win import *
 from second_win import *
-
-
-class Expirement():
-    def __init__(self, age, test1, test2, test3):
-        self.age = age
-        self.t1 = test1
-        self.t2 = test2
-        self.t3 = test3
-        
-
 
 
 class MainWin(QWidget):
     def __init__(self):
         super().__init__()
-        self.set_appear()
         self.initUI()
         self.connects()
-        self.show()
+        self.set_appear()
 
+        self.show()
     def set_appear(self):
         self.setWindowTitle(txt_title)
         self.resize(win_width, win_height)
         self.move(win_x, win_y)
-
     def initUI(self):
         self.hello_text = QLabel(txt_hello)
         self.instruction = QLabel(txt_instruction)
-        self.button = QPushButton(txt_next)
-        self.layout = QVBoxLayout()
-        self.layout.addWidget(self.hello_text, alignment=Qt.AlignCenter)
-        self.layout.addWidget(self.instruction, alignment=Qt.AlignCenter)
-        self.layout.addWidget(self.button, alignment=Qt.AlignCenter)
-        self.setLayout(self.layout)
-        
+        self.button = QPushButton(txt_next) 
+        self.layout_line = QVBoxLayout()
+        self.layout_line.addWidget(self.hello_text, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.instruction, alignment = Qt.AlignLeft)
+        self.layout_line.addWidget(self.button, alignment = Qt.AlignCenter)
+        self.setLayout(self.layout_line)
+    
+    def next_click(self):
+        self.tw = TestWin()
+        self.hide()
+
     def connects(self):
         self.button.clicked.connect(self.next_click)
 
-    def next_click(self):
-        self.hide()
 
 
-        self.tw = TestWin()
-        class TestWin(QWidget):
-            def next_click(self):
-                self.hide()
-                self.exp = Expirement(self.line_age.text(),self.line_test1.text(),self.line_test2.text(),self.line_test3.text())
-                self.tw = FinalWin(self.exp)
 
-app = QApplication([]) #Создание приложения
-zx=MainWin()
-zx.initUI()
 
-app.exec_() #Пока не нажат крестик - приложение работает(Всегда в конце)
+app = QApplication([])
+mw = MainWin()
+app.exec_()
